63c0721e35a12657befea46f45f1625c7c2eb568
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-18 18:42:54 +0000

    added carrousel

---
+++ src/components/featured/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carrousel from './Carrousel'
+
+const Featured = () => {
+    return (
+        <div style={{position:'relative'}}>  
+
+        <Carrousel/>
+
+        <div className="artist_name">
+            <div className="wrapper">
+                Jesus and the Mary Chain
+            </div>
+
+
+        </div>
+
+        </div>
+    );
+};
+
+export default Featured;
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
     }
 
 
-    componentDidMount(){
+    UNSAFE_componentWillMount(){
         window.addEventListener('scroll',this.handleScroll);
     }
 
