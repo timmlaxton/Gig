b0739864ad478f7607a83359a68ad4ddff8477f6
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-19 13:30:27 +0000

    added button props

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
+import Button from '../UI/MyButton';
+
 class Discount extends Component {
 
     state = {
@@ -45,12 +47,16 @@ componentDidUpdate(){
                     <div className="discount_description">
                         <h3> Summer nights pledgs to donate 10% from each ticket sold to help teach music to children throughout Glasgow.   </h3>
 
-                        </div>
+                        
 
-                    <div>                   
-                        button
+                    <Button
+                        text="Purchase tickets"
+                        bck="pink"
+                        color="white"
+                        link="https://www.ticketmaster.co.uk/artist/1992315?irgwc=1&utm_source=1296104-What%27s%20On%20Network&utm_medium=affiliate&utm_campaign=1296104&awtrc=&clickId=RkB210SxmxyJWfewUx0Mo3E1UknStKR9nTc%3AVc0"
+                    />
+
                     </div>
-
 
                    
 
